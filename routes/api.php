2b74effb95ac5d94dad6877b3b67fd7436a586f4
febri2001 <febriye12@gmail.com>
2021-11-04 19:38:59 +0700

    Laravel Pusher

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\{ MeController, UserController, ConversationController };
+use App\Http\Controllers\Auth\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('me', MeController::class);
+
+    Route::get('user', [UserController::class, 'index']);
+    Route::get('user/{id}', [UserController::class, 'show']);
+
+    Route::get('conversation/{user_two}', [ConversationController::class, 'show'])->name('conversation.show');
+    Route::post('conversation/{conversation}/message', [ConversationController::class, 'store'])->name('conversation.store');
 });
